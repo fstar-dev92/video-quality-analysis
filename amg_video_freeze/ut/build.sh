3eafe56b3a4893865a7269c28d69da94f0c4b1b0
bvs-amagi <viswanath.b@amagi.com>
2021-05-21 11:04:41 +0530

    removing amg_video_freeze

--- amg_video_freeze/ut/build.sh
+++
@@ -1,3 +0,0 @@
-gcc $(dirname "$0")/amg_video_freeze_detect_ut1.c -g -lippi -lipps -lippcore -lippcc -lippvm -lippcv -lm -I/opt/intel/ipp/include -I./inc -L/opt/intel/ipp/lib/intel64 -o $(dirname "$0")/amg_freeze_detect_ut1
-gcc $(dirname "$0")/amg_video_black_detect_ut.c -g -lippi -lipps -lippcore -lippcc -lippvm -lippcv -lm -I/opt/intel/ipp/include -I./inc -L/opt/intel/ipp/lib/intel64 -o $(dirname "$0")/amg_black_detect
-
